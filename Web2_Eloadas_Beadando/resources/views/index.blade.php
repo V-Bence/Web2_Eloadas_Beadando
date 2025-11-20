172ddb2dad176dad34a9ef088871b2f2e7d437a1
FLBence <bence2405.fb@gmail.com>
2025-11-20 16:41:22 +0100

    reszponzív téma hozzáadása

---
+++ Web2_Eloadas_Beadando/resources/views/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+{{-- resources/views/index.blade.php --}}
+@section('content')
+<h1 id="maintitle">Beadandó</h1>
+
+<nav id="mainMenu">
+    <a href="{{ route('fooldal') }}">Főoldal</a>
+    <a href="{{ route('adatbazis') }}">Adatbázis</a>
+    <a href="{{ route('crud.index') }}">CRUD</a>
+</nav>
+
+<div id="userMenu">
+    @guest
+        <a href="{{ route('login') }}">Bejelentkezés</a>
+        <a href="{{ route('register') }}">Regisztráció</a>
+    @endguest
+
+    @auth
+        <a href="{{ route('uzenetek') }}">Üzenetek</a>
+        <a href="{{ route('kapcsolat') }}">Kapcsolat</a>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit">Kijelentkezés</button>
+        </form>
+
+        @if(auth()->user()->role === 'admin')
+           <a href="{{ route('admin.index') }}">Admin</a>
+        @endif
+    @endauth
+</div>
+@endsection
--- Web2_Eloadas_Beadando/routes/web.php
+++ Web2_Eloadas_Beadando/routes/web.php
@@ -1,31 +1,57 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DatabaseController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\UzenetController;
+use App\Http\Controllers\PilotaCrudController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DiagramController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/fooldal', [HomeController::class, 'fooldal'])->name('fooldal');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/adatbazis', [DatabaseController::class, 'index'])->name('adatbazis');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/kapcsolat', [ContactController::class, 'create'])->name('kapcsolat');
+Route::post('/kapcsolat', [ContactController::class, 'store']);
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/uzenetek', [UzenetController::class, 'index'])->name('uzenetek');
+
+    // Diagram menü
+    Route::get('/diagram', [DiagramController::class, 'index'])->name('diagram');
+
+    // CRUD Pilota
+    Route::get('/crud', [PilotaCrudController::class, 'index'])->name('crud.index');
+    Route::get('/crud/create', [PilotaCrudController::class, 'create'])->name('crud.create');
+    Route::post('/crud', [PilotaCrudController::class, 'store'])->name('crud.store');
+    Route::get('/crud/{az}/edit', [PilotaCrudController::class, 'edit'])->name('crud.edit');
+    Route::put('/crud/{az}', [PilotaCrudController::class, 'update'])->name('crud.update');
+    Route::delete('/crud/{az}', [PilotaCrudController::class, 'destroy'])->name('crud.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+
+Route::get('/register', [RegisteredUserController::class, 'create'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->middleware('guest');
+
+// Admin menü
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
